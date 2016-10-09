22f0bd392d32870a95cf38849bee0362472e7d0b
EvanYap <evanyap.14@gmail.com>
2016-10-09 12:27:14 -0700

    make new branch and edit code

--- Dummy/src/Dummy.java
+++ Dummy/src/Dummy.java
@@ -2,7 +2,7 @@
 public class Dummy {
 
   public static void main(String[] args) {
-    System.out.println("Dumbass");
+    System.out.println("warren is the Dumbass");
     // Warren is the dumbass
   }
 
